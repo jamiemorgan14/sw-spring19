11aee77bf8a84eb9c4b23f0082f55dc4093c68e3
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-02-18 12:09:51 -0700

    people working

---
+++ app/main.js
@@ -0,0 +1,13 @@
+import StarWarsController from "./components/starWarsController.js";
+
+class App {
+    constructor() {
+        this.controllers = {
+            swController: new StarWarsController()
+        }
+    }
+}
+
+
+
+window['app'] = new App()
